53a369dfb7f5ff20421242c1aa41fa7f75c51823
root <jiaofangduchongfei@gmail.com>
2023-11-17 14:50:37 +0800

    redis数据库测试

---
+++ serdis-test/src/main/java/com/fallsea/test/Test1.java
@@ -0,0 +1,15 @@
+package com.fallsea.test;
+
+/**
+ * 1
+ * 2	* @author FallSea
+ * 3	* @date 2023年11月17日 14:49
+ * 4
+ */
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("daggffgf");
+    }
+}
